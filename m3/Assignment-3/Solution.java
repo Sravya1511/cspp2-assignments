4b94ebd0ff932f5e56c0f8bed043e98658a85b21
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-29 14:24:52 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 /*
 	Do not modify this main function.
@@ -11,9 +10,22 @@ public class Solution {
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1,n2);
-        System.out.println(gcd);
+        System.out.println(gcd(n1, n2));
     }
     /*
 	Need to write the gcd function and print the output.
 	*/
+	static int gcd(int n1, int n2) {
+		while(n2 != 0) { 
+			int r = n1%n2;
+			if (r == 0)
+				return n2;
+			else {
+				n1 = n2;
+				n2 = r;
+			}
+		}
+		return n2;
+	}
+
 }
