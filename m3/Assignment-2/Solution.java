663935375caf29bcb6ec787ba0391276edada365
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-29 14:39:21 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-2/Solution.java
+++ m3/Assignment-2/Solution.java
@@ -11,7 +11,7 @@ final class Solution {
     /**
     * @param args comment.
     */
-/* Fill the main function to print the number of 7's between 1 to n*/
+
     public static void main(final String[] args) {
 
         Scanner s = new Scanner(System.in);
