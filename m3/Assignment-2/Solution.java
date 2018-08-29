25cbc2ad18a58524f701ca529854639125b74c11
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-29 14:35:49 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-2/Solution.java
+++ m3/Assignment-2/Solution.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
     *Do not modify this main function.
     */
 final class Solution {
+   /**
+    *Do not modify this main function.
+    */
+    private Solution() { }
+    /**
+    * @param args comment
+    */
 /* Fill the main function to print the number of 7's between 1 to n*/
     public static void main(final String[] args) {
 
@@ -11,6 +18,13 @@ final class Solution {
         int n = s.nextInt();
         System.out.println(numberOf(n));
     }
+    /**
+     * { function_description }
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
 
     static int numberOf(final int n) {
         int count = 0;
