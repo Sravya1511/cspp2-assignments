fee8d7bd7d96b03fe03b9169a9ec430282486d34
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-30 22:54:23 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -9,10 +9,10 @@ public class Solution
 	*/
 	public static void main(String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=0;i<=n;i++){
-			String s=sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i <= n; i++){
+			String s = sc.nextLine();
 			binaryToDecimal(s);//Write binaryToDecimal function
 			// System.out.println(res);
 			System.out.println();
@@ -22,12 +22,12 @@ public class Solution
 		
 	  double sum =  0;
       int count = 0;
-      for(int i=s.length()-1; i>=0 ;i--) {
+      for(int i = s.length() - 1; i >= 0 ;i--) {
         char ch = s.charAt(i);
         // System.out.println("Character at "+i+" Position: "+ch);
         int x = Character.getNumericValue(ch);
         // System.out.println(x);
-        sum = sum+(x*Math.pow(2,count));
+        sum = sum + (x * Math.pow(2,count));
         count++;
       } 
       System.out.format("%.0f", sum);
