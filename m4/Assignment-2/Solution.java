447d8d853eed20a8f15c32de9a252505214a48e7
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-30 15:03:00 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -1,12 +1,13 @@
-/**This is a program.**/
 import java.util.*;
+/**This is a program.**/
 final class Solution {
-    /* Fill the main function to print resultant of addition of matrices*/
+     /**
+    *Do not modify this main function.
+    */
+    private Solution() { }
     /**
-     * { function_description }.
-     *
-     * @param      args  The arguments
-     */
+    * @param args comment
+    */
     public static void main(final String[] args) {
 
         // System.out.println("Enter size of array 1");
@@ -28,17 +29,16 @@ final class Solution {
         int[][] arr2;
         arr2 = new int[r2][c2];
         for (int i = 0; i < arr2.length; i++) {
-            for (int j = 0; j < arr2[i].length; j++){
+            for (int j = 0; j < arr2[i].length; j++) {
                 arr2[i][j] = input.nextInt();
 
             }
         }
 
-        
         int[][] arr3;
         arr3 = new int[r1][c1];
         if ((r1 != r2) || (c1 != c2)) {
-                    System.out.println("not possible");
+            System.out.println("not possible");
         }
         else {
         for (int i = 0; i < arr1.length; i++) {
@@ -46,8 +46,7 @@ final class Solution {
                 arr3[i][j] = arr1[i][j] + arr2[i][j];
                 if (j != arr1[i].length - 1) {
                     System.out.print(arr3[i][j] + " ");
-                }
-                else {
+                } else {
                     System.out.print(arr3[i][j]);
                 }
             }
