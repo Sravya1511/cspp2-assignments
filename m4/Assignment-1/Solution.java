f88f3666003469795281b0325506e3c86752dac2
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-30 11:57:43 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -12,7 +12,7 @@ public class Solution
 		int temp = 0;
 		for (int i=0; i<arr.length; i++) {
 			arr[i] = sc.nextInt();
-			if (temp > arr[i]) {
+			if (temp < arr[i]) {
 				temp = arr[i];
 			}
 		}
