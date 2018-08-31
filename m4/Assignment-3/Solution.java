5103edc6597404940b8cd5ff6be579ac670ce4ad
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-31 14:21:31 +0530

    "check_style_score -> 0 of 1 passed. style score: 1/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -30,14 +30,14 @@ final class Solution {
     static String binaryToDecimal(final String s) {
       double sum =  0;
       int count = 0;
-      for (int i = s.length() - 1; i >= 0 ; i--) {
+      for (int i = s.length() - 1; i >= 0; i--) {
         char ch = s.charAt(i);
         // System.out.println("Character at "+i+" Position: "+ch)
         int x = Character.getNumericValue(ch);
         // System.out.println(x);
         sum = sum + (x * Math.pow(2, count));
         count++;
-      } 
+      }
       return String.format("%.0f", sum);
     }
 }
