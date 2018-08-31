ebf088b505020f2da14781ca61ff7ae6b6977d4f
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-31 14:20:31 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -12,34 +12,32 @@ final class Solution {
     * @param args comment
     */
 
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i = 0; i <= n; i++) {
-			String s = sc.nextLine();
-			String res = binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      s     { parameter_description }.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	static String binaryToDecimal(final String s) {
-		
-	  double sum =  0;
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);
+        }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static String binaryToDecimal(final String s) {
+      double sum =  0;
       int count = 0;
-      for(int i = s.length() - 1; i >= 0 ; i--) {
+      for (int i = s.length() - 1; i >= 0 ; i--) {
         char ch = s.charAt(i);
-        // System.out.println("Character at "+i+" Position: "+ch);
+        // System.out.println("Character at "+i+" Position: "+ch)
         int x = Character.getNumericValue(ch);
         // System.out.println(x);
         sum = sum + (x * Math.pow(2, count));
         count++;
       } 
       return String.format("%.0f", sum);
-	}
+    }
 }
