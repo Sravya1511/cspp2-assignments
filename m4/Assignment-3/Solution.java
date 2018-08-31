f657f9850d92f77269519c03741fc8b97db186dc
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-31 13:55:49 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -15,7 +15,7 @@ public class Solution
 			String s = sc.nextLine();
 			binaryToDecimal(s);//Write binaryToDecimal function
 			// System.out.println(res);
-			System.out.println();
+			// System.out.println(res);
 		}
 	}
 	static void binaryToDecimal(String s) {
