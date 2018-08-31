188a5a69c528073027b7391bbac5404d275c92db
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-31 14:17:58 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -1,32 +1,43 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
+/*This is a program.*/
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
+/**
+ * Class for solution.
+ */
+final class Solution {
+     /**
+    *Do not modify this main function.
+    */
+    private Solution() { }
+    /**
+    * @param args comment
+    */
+
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		for(int i = 0; i <= n; i++){
+		for(int i = 0; i <= n; i++) {
 			String s = sc.nextLine();
 			String res = binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
-	static String binaryToDecimal(String s) {
+	/**
+	 * { function_description }
+	 *
+	 * @param      s     { parameter_description }.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	static String binaryToDecimal(final String s) {
 		
 	  double sum =  0;
       int count = 0;
-      for(int i = s.length() - 1; i >= 0 ;i--) {
+      for(int i = s.length() - 1; i >= 0 ; i--) {
         char ch = s.charAt(i);
         // System.out.println("Character at "+i+" Position: "+ch);
         int x = Character.getNumericValue(ch);
         // System.out.println(x);
-        sum = sum + (x * Math.pow(2,count));
+        sum = sum + (x * Math.pow(2, count));
         count++;
       } 
       return String.format("%.0f", sum);
