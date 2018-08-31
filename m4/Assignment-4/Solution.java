589332cae35e4b47df5eafebf6f2f48b79e45a05
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-08-31 14:27:22 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -1,38 +1,36 @@
 /**This is a program.*/
 import java.util.*;
-import java.util.Scanner;
 final class Solution {
      /**
     *Do not modify this main function.
     */
     private Solution() { }
     /**
-    * @param args comment
+    * @param args comment.
     */
 
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      s     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	static String reverseString(final String s) {
-		String rev = "";
-		for (int i = s.length() - 1; i >= 0; i--) {
-			char c = s.charAt(i);
-			String str = Character.toString(c);
-			rev = rev+str;
-		}
+    public static void main(final String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static String reverseString(final String s) {
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            String str = Character.toString(c);
+            rev = rev + str;
+        }
     return rev;
-	}
-	//Write reverseString function
+    }
+    //Write reverseString function
 }
