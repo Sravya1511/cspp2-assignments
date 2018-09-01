315161a6be247392d1e52aa27bc285149cf9282d
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-01 09:52:25 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- M6/m6/Assignment-1/Solution.java
+++ M6/m6/Assignment-1/Solution.java
@@ -9,7 +9,7 @@ final class Solution {
     /**
     * Empty constructor.
     */
-    private Solution() {
+    private Solution( ) {
         //not used
     }
     /**
@@ -18,21 +18,21 @@ final class Solution {
      * @param      n     n value
      */
     static void oddComposites(final int n) {
-	// write your code here
-    	for ( int i = 2; i<n; i++) {
-            if (i%2 != 0) {
-    			int count = 0;
-    			for ( int j = 1; j<= i; j++) {
-    				if(i%j == 0) {
-    					count = count+1;
-    				}
-    			}
-    			if (count != 2 ) {
-    				System.out.println(i);
-    			}
+    // write your code here
+        for ( int i = 2; i < n; i++) {
+            if (i % 2 != 0) {
+                int count = 0;
+                for ( int j = 1; j <= i; j++) {
+                    if (i % j == 0) {
+                        count = count + 1;
+                    }
+                }
+                if (count != 2 ) {
+                    System.out.println(i);
+                }
 
-    		}
-    	}
+            }
+        }
     }
 
     /**
