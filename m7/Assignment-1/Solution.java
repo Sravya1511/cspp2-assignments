7676010b01829cc30263c98094406a43eb7ad070
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-03 18:14:03 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -12,20 +12,18 @@ class InputValidator
 
 	boolean validateData() {
 		int l = input.length();
-        if(l>=6) {
+        if(l >=6 ) {
         	return true;
         } else { return false; }
-
-
 	}
 }
 public class Solution
 {
 	public static void main(String args[])
     {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
+    	Scanner s = new Scanner(System.in);
+    	String input = s.next();
+    	InputValidator i = new InputValidator(input);    	
     	System.out.println(i.validateData());
 
     }
