afbbfef486700e1385e73bea417a0527615b15e7
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-03 18:33:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,43 +1,44 @@
 /*This is a program.*/
 import java.util.Scanner;
 final class InputValidator {
-	/*Write the atrributes and methods for InputValidator*/
-	/**
-	 * { var_description }
-	 */
-	String input;
+    /*Write the atrributes and methods for InputValidator*/
+    /**
+     * { var_description }
+     */
+    String input;
 
 
 
-	InputValidator(final String input) {
-		this.input = input;
-	}
+    InputValidator(final String input) {
+        this.input = input;
+    }
     /**
      * { function_description }
      *
      * @return     { description_of_the_return_value }
      */
-	boolean validateData() {
-		int l = input.length();
+    boolean validateData() {
+        int l = input.length();
         if (l >= 6) {
-        	return true; }
-    
-        return false; 
-        
-	}
+            return true; 
+        }
+
+        return false;
+
+    }
 }
 /*Program.*/
 final class Solution {
-	/**
-	 * '{ item_description }.
-	 */
-	private Solution() {
-		
-	}
-	public static void main(final String args[]) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+    /**
+     * '{ item_description }.
+     */
+    private Solution() {
+
+    }
+    public static void main(final String args[]) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
     }
 }
