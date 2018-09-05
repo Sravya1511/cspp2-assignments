f15345df90f1128dab80fab66667bde180e433c6
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-05 23:19:15 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- m8/CodecampList/Homework/List.java
+++ m8/CodecampList/Homework/List.java
@@ -286,7 +286,7 @@ public class List {
         if (index < 0) {
             System.out.println("Negative Index Exception");
         }
-        if (index == 0) {
+        else if (index == 0) {
             list[size++] = item;
         }
         else {
