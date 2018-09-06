f722694bf714defb515b456aa359040236bf5920
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-06 14:36:04 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- m10/Assignment-1/List.java
+++ m10/Assignment-1/List.java
@@ -1,6 +1,6 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
-import java.util.*;
+import java.util.Arrays;
 /**program starts.**/
 final class List {
     //Implement all the methods mentioned to build a ListADT
