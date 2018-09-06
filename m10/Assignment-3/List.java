abc5f21c30d1111e014064651441b5911ebc1246
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-06 14:58:50 +0530

    "check_style_score -> 5 of 6 passed. style score: 0/1 "

--- m10/Assignment-3/List.java
+++ m10/Assignment-3/List.java
@@ -168,7 +168,7 @@ public class List<E> {
     public boolean contains(E item) {
 		//Write logic for contains method
         for (int i = 0; i < list.length; i++) {
-            if (list[i].equals(item)) {
+            if (list[i] == item) {
             return true;
             }
         }
