491768b58f1311c1d51d2a6e17a03a680acaf38c
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-12 17:40:32 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m16/bookyourshow/BookYourShow.java
+++ m16/bookyourshow/BookYourShow.java
@@ -1,4 +1,45 @@
 import java.util.*;
+class Show {
+	String nameOfMovie;
+	String date;
+	String[] list;
+	Patron[] bookedSeats;
+
+	Show (String n, String d, String[] l) {
+		nameOfMovie = n;
+		date = d;
+		list = l;
+		bookedSeats = new Patron[l.length];
+	}
+
+	public String toString() {
+        StringBuilder sb = new StringBuilder();
+            sb.append(nameOfMovie + ",");
+            sb.append(date + ",");
+            sb.append("[");
+            for(int i = 0; i<list.length-1; i++) {
+            	sb.append(list[i] + ",");
+            }
+            sb.append(list[list.length-1] + "]");
+        return sb.toString();
+    }
+}
+class Patron {
+	String user;
+	String mobile;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      u     { parameter_description }
+	 * @param      m     { parameter_description }
+	 */
+
+	Patron(final String u, final String m) {
+		user = u;
+		mobile = m;
+	}
+}
+
 public class BookYourShow {
 	Show[] showsList;
 	int size;
