05daa469f15d598119094e96b97642daf2278246
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-12 16:51:38 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- m16/bookyourshow/BookYourShow.java
+++ m16/bookyourshow/BookYourShow.java
@@ -33,7 +33,6 @@ public class BookYourShow {
 	}
 
 	public void bookAShow(String movieName, String date, Patron p, String[] listTic) {
-		boolean x = false;
 		for(Show s : showsList) {
 			if(s!=null) {
 		        if(s.nameOfMovie.equals(movieName) && s.date.equals(date)) {
@@ -42,15 +41,12 @@ public class BookYourShow {
             		    if(s.list[j].equals(seatNumber)) {
             			s.list[j] = "N/A";
             			s.bookedSeats[j] = p;
-            			x = true;
+
             		    }
             	    }
                 }
 		        }
             }
-	    }
-	    if(x == false) {
-	    	System.out.println("No show");
 	    }
 	}
 
