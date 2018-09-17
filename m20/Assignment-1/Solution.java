b4ed8f10b0fd031edf2979872a3a7d25e384569f
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-17 17:43:59 +0530

    "check_style_score -> 7 of 13 passed. style score: 0/1 "

--- m20/Assignment-1/Solution.java
+++ m20/Assignment-1/Solution.java
@@ -298,6 +298,11 @@ public final class Solution {
             	System.out.println("Error! Correct answer choice number is out of range for question text 1");
             	return;
             }
+            if(list3.length < 4) {
+            	b = true;
+            	System.out.println("trick question  does not have enough answer choices");
+            	return;
+            }
             x = list1.length + list2.length;
  quiz.addQuestion(new Question(list1[0], list3, Integer.parseInt(list1[2]),
     Integer.parseInt(list1[three]), Integer.parseInt(list1[four])));
