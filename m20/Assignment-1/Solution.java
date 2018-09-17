3d579eaa1edc4b61bd0fd90f4f683241b5cabd50
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-17 19:58:43 +0530

    "check_style_score -> 13 of 13 passed. style score: 0/1 "

--- m20/Assignment-1/Solution.java
+++ m20/Assignment-1/Solution.java
@@ -7,6 +7,9 @@ public final class Solution {
 	 * Constructs the object.
 	 */
 	static boolean b = false;
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 		// leave this blank
 	}
@@ -26,19 +29,18 @@ public final class Solution {
 			String line = s.nextLine();
 			 // split the line using space
 			String[] tokens = line.split(" ");
-			  // based on the list operation invoke the corresponding method
 			switch (tokens[0]) {
 				case "LOAD_QUESTIONS":
 				System.out.println("|----------------|");
 				System.out.println("| Load Questions |");
 				System.out.println("|----------------|");
-				loadQuestions(s, q, Integer.parseInt(tokens[1]));
+		loadQuestions(s, q, Integer.parseInt(tokens[1]));
 				break;
 				case "START_QUIZ":
 				System.out.println("|------------|");
 				System.out.println("| Start Quiz |");
 				System.out.println("|------------|");
-					startQuiz(s, q, Integer.parseInt(tokens[1]));
+			startQuiz(s, q, Integer.parseInt(tokens[1]));
 
 
 				break;
@@ -80,33 +82,33 @@ public final class Solution {
 			String sen = scan.nextLine();
 			String[] list1 = sen.split(":");
 			String[] list2 = list1[1].split(",");
-			if(list2.length < 2) {
+			if (list2.length < 2) {
 				b = true;
-				System.out.println("trick question  does not have enough answer choices");
+System.out.println("trick question  does not have enough answer choices");
 				return;
 			}
 			// String [] list3 = {list2[0], list2[1], list2[2], list2[three]};
-			if(list1.length < 5) {
+			if (list1.length < 5) {
 				b = true;
 				System.out.println("Error! Malformed question");
 				return;
 			}
-			if(Integer.parseInt(list1[2]) > 4) {
+			if (Integer.parseInt(list1[2]) > 4) {
 				b = true;
 				System.out.println("Error! Correct answer choice number is out of range for question text 1");
 				return;
 			}
-			if(Integer.parseInt(list1[three]) < 0) {
-				b= true;
+			if (Integer.parseInt(list1[three]) < 0) {
+				b = true;
 				System.out.println("Invalid max marks for "+list1[0]);
 				return;
 			}
-			if(Integer.parseInt(list1[four]) > 0) {
+			if (Integer.parseInt(list1[four]) > 0) {
 				b = true;
-				System.out.println("Invalid penalty for "+list1[0]);
+				System.out.println("Invalid penalty for " + list1[0]);
 				return;
 			}
-			if(list1[0].equals("")) {
+			if (list1[0].equals("")) {
 				b = true;
 				System.out.println("Error! Malformed question");
                 return;
@@ -116,7 +118,7 @@ public final class Solution {
  quiz.addQuestion(new Question(list1[0], list2, Integer.parseInt(list1[2]),
 	Integer.parseInt(list1[three]), Integer.parseInt(list1[four])));
 		}
-		System.out.println(q+ " are added to the quiz");
+		System.out.println(q + " are added to the quiz");
 
 	}
 
@@ -130,14 +132,13 @@ public final class Solution {
 	 */
 	public static void startQuiz(final Scanner scan,
 		final Quiz quiz, final int q) {
-		// write your code here to display the quiz questions on the console.
-		// read the user responses from the console using scanner object.
+
+
 		// store the user respone in the question object
 		final int four = 4;
-		if(b == true) {
+		if (b == true) {
 			return;
-		}
-		else {
+		} else {
 		for (int i = 0; i < q; i++) {
 			quiz.printQuestion(i);
 			String m = scan.nextLine();
@@ -151,11 +152,9 @@ public final class Solution {
 	 * @param      quiz     The quiz object
 	 */
 	public static void displayScore(final Quiz quiz) {
-		// write your code here to display the score report using quiz object.
-		if(b == true) {
+		if (b == true) {
 			return;
-		}
-		else {
+		} else {
 			quiz.showReport();
 		}
 	}
