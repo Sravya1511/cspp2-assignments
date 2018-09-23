0c8ce16f987199d83332eb4c5303a78e6e82999e
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-23 20:19:03 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- m24/Stringmatching/Solution.java
+++ m24/Stringmatching/Solution.java
@@ -8,68 +8,6 @@ import java.util.Map;
 import java.util.*;
 import java.io.*;
 
-// class BagOfWords {
-// 	Map<String, Integer> dictionary1 = new HashMap<String, Integer>();
-// 	Map<String, Integer> dictionary2 = new HashMap<String, Integer>();
-// 	Map<String, List<Integer>> dictionary3 = new HashMap<String, List<Integer>>();
-// 	BagOfWords() {
-// 	}
-
-// 	public void addDic1(String[] list) {
-//         for(String i : list) {
-//          	if(dictionary1.containsKey(i)) {
-//          		int k = dictionary1.get(i);
-//          		dictionary1.put(i, k+1);
-//          	}
-//          	else {
-//          		dictionary1.put(i, 1);
-//          	}
-//         }
-//         // for(String key: dictionary1.keySet())
-//         // System.out.println(key+" "+dictionary1.get(key));
-// 	}
-// 	public void addDic2(String[] list) {
-//         for(String i : list) {
-//          	if(dictionary2.containsKey(i)) {
-//          		int k = dictionary2.get(i);
-//          		dictionary2.put(i, k+1);
-//          	}
-//          	else {
-//          		dictionary2.put(i, 1);
-//          	}
-//         }
-//         // for(String key: dictionary2.keySet())
-//         // System.out.println(key+" "+dictionary2.get(key));
-// 	}
-
-// 	public double getDistance() {
-// 		float num = 0;
-// 		float a = 0;
-// 		double den = 0;
-//         double res;
-// 		for(String i:dictionary1.keySet()){
-// 			if(dictionary2.containsKey(i)) {
-//                 num += dictionary1.get(i)*dictionary2.get(i);
-// 			}
-// 		}
-// 		for(String i:dictionary1.keySet()) {
-//             a += Math.pow(dictionary1.get(i), 2);
-// 		}
-// 		float c = 0;
-// 		for(String i:dictionary2.keySet()) {
-//             c += Math.pow(dictionary2.get(i), 2);
-// 		}
-// 		den = Math.sqrt(a)*Math.sqrt(c);
-// 		res = (num/den)*100;
-// 		//System.out.print("HHHHHH"+res);
-//         if (Double.isNaN(res)) {
-//         	return 0;
-//         }
-//         else {
-//         	return res;
-//         }
-// 	}
-// }
 
 class StringMatching {
 	String l1;
@@ -173,7 +111,7 @@ class Solution {
 		    			String[] tokens = line.toLowerCase().split(" ");
 
                         } catch (NoSuchElementException e) {
-        	                System.out.print("         0.0");
+        	                System.out.print("          0.0");
                         }
                         catch (NullPointerException e) {
                         	System.out.print("");
@@ -197,7 +135,7 @@ class Solution {
 
                         }
                         catch (NoSuchElementException e) {
-        	                System.out.print("        0.0");
+        	                System.out.print("          0.0");
                         }
                         catch (NullPointerException e) {
                         	System.out.print("");
