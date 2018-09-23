a6f08ff81f4ebd9cdce633c750044ef6e2654968
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-23 15:04:56 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- m24/Bagofwords/Solution.java
+++ m24/Bagofwords/Solution.java
@@ -71,54 +71,54 @@ class BagOfWords {
 	}
 }
 
-class StringMatching {
-	String l1;
-	String l2;
-	int len1;
-	int len2;
-	StringMatching() {
+// class StringMatching {
+// 	String l1;
+// 	String l2;
+// 	int len1;
+// 	int len2;
+// 	StringMatching() {
 
-	}
-	public void str1(String tokens) {
-		l1 = tokens;
-		len1 = tokens.length();
-		System.out.println(len1);
-	}
-	public void str2(String tokens1) {
-		l2 = tokens1;
-		len2 = tokens1.length();
-		System.out.println(len2);
-	}
-	 public void compare() {
-	 	double total = len1+len2;
-	 	double max = 0;
-        for(int i = 0; i<l1.length(); i++) {
-        	for(int j = 0; j<l2.length(); j++) {
-        		int k = 0;
-        		if(l1.charAt(i) == l2.charAt(j)) {
-        			double c = 1;
-        			while((k+i)<l1.length() && (k+j)<l2.length()) {
-        				if(l1.charAt(i+k) == l2.charAt(j+k)) {
-        					c++;
-        					if(c>max) {
-        						max = c;
-        					}
-        				}
-        				else{
-        					c = 0;
-        					break;
-        				}
-        				k++;
-        			}
-        		}
-        	}
-        }
-        System.out.println(max);
-        double res = (max*2)/total;
-        double res1 = res*100;
-        System.out.println(res1);
-	}
-}
+// 	}
+// 	public void str1(String tokens) {
+// 		l1 = tokens;
+// 		len1 = tokens.length();
+// 		System.out.println(len1);
+// 	}
+// 	public void str2(String tokens1) {
+// 		l2 = tokens1;
+// 		len2 = tokens1.length();
+// 		System.out.println(len2);
+// 	}
+// 	 public void compare() {
+// 	 	double total = len1+len2;
+// 	 	double max = 0;
+//         for(int i = 0; i<l1.length(); i++) {
+//         	for(int j = 0; j<l2.length(); j++) {
+//         		int k = 0;
+//         		if(l1.charAt(i) == l2.charAt(j)) {
+//         			double c = 1;
+//         			while((k+i)<l1.length() && (k+j)<l2.length()) {
+//         				if(l1.charAt(i+k) == l2.charAt(j+k)) {
+//         					c++;
+//         					if(c>max) {
+//         						max = c;
+//         					}
+//         				}
+//         				else{
+//         					c = 0;
+//         					break;
+//         				}
+//         				k++;
+//         			}
+//         		}
+//         	}
+//         }
+//         System.out.println(max);
+//         double res = (max*2)/total;
+//         double res1 = res*100;
+//         System.out.println(res1);
+// 	}
+// }
 /**
  * Class for solution..
  */
