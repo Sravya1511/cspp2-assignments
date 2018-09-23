91fddfbcbd73cddccf4ab43e78d484d1fbf79057
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-23 21:34:34 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- m24/Bagofwords/Solution.java
+++ m24/Bagofwords/Solution.java
@@ -1,13 +1,12 @@
-// import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.File;
-// import java.io.FileReader;
+import java.io.FileReader;
 import java.io.IOException;
-// import java.util.HashMap;
-// import java.util.Hashtable;
-// import java.util.Map;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.*;
 import java.io.*;
-import java.util.NoSuchElementException;
 
 
 /**
@@ -25,7 +24,7 @@ class Solution {
 	/**
 	 * Constructs the object.
 	 */
-	private Solution() {
+	Solution() {
 
 	}
 	/**
@@ -40,62 +39,62 @@ class Solution {
 		int a = 0;
 		int b = 0;
 		StringMatching match = new StringMatching();
-		Scanner input = new Scanner(System.in);
-		try {
-			String x = input.nextLine();
-			File file = new File(x);
+        Scanner input = new Scanner(System.in);
+        try {
+        	String x = input.nextLine();
+        	File file = new File(x);
 			File[] files = file.listFiles();
-			System.out.print("              ");
+			System.out.print("      		");
 			for (int i = 1; i <= files.length; i++) {
-				System.out.print("File" + i + ".txt" + "    ");
+				System.out.print("File" + i + ".txt" + "	");
 			}
 			System.out.println();
 
-			for (int i = 0; i < files.length; i++) {
-				int h = i + 1;
-				   System.out.print("File" + h + ".txt" + " ");
-				for (int j = 0; j < files.length; j++) {
-						BagOfWords bags = new BagOfWords();
-						// StringMatching match = new StringMatching();
-						try {
-							Scanner s = new Scanner(files[i]);
-						String line = s.useDelimiter("\\A").next();
-						// match.str1(line);
-						String[] tokens = line.toLowerCase().split(" ");
-						bags.addDic1(tokens);
-						} catch (Exception e) {
-						   System.out.print("NoFile          ");
-						}
-						try {
-							Scanner s1 = new Scanner(files[j]);
-						String line1 = s1.useDelimiter("\\A").next();
-						// match.str2(line1);
-						String[] tokens1 =
-						line1.toLowerCase().split(" ");
-						bags.addDic2(tokens1);
-						} catch (Exception e) {
-							System.out.print("NoFile");
-						}
-						if (bags.getDistance() != 0) {
-						double p = bags.getDistance();
-						System.out.format("%.0f", p);
-						System.out.print("      ");
-						if (p > max && i != j) {
-							 a = i + 1;
-							 b = j + 1;
-							 max = p;
-							}
-						}
-					// match.compare();
-				}
-				System.out.println();
-			}
-			System.out.println("Maximum similarity is between " + "File" + a
-				+ ".txt and File" + b + ".txt");
+		    for (int i = 0; i < files.length; i++) {
+		    	int h = i + 1;
+                   System.out.print("File" + h + ".txt" + "	");
+		    	for (int j = 0; j < files.length; j++) {
+                        BagOfWords bags = new BagOfWords();
+                        // StringMatching match = new StringMatching();
+                        try {
+                        	Scanner s = new Scanner(files[i]);
+		    			String line = s.useDelimiter("\\A").next();
+		    			// match.str1(line);
+		    			String[] tokens = line.toLowerCase().split(" ");
+		    			bags.addDic1(tokens);
+                        } catch (Exception e) {
+                           System.out.print("NoFile          ");
+                        }
+                        try {
+                        	Scanner s1 = new Scanner(files[j]);
+		    			String line1 = s1.useDelimiter("\\A").next();
+		    		    // match.str2(line1);
+		    			String[] tokens1 =
+		    			line1.toLowerCase().split(" ");
+		    			bags.addDic2(tokens1);
+                        } catch (Exception e) {
+                        	System.out.print("NoFile");
+                        }
+                        if (bags.getDistance() != 0) {
+                        double p = bags.getDistance();
+		    			System.out.format("%.0f", p);
+		    			System.out.print("		");
+		    			if (p > max && i != j) {
+		    				 a = i + 1;
+		    				 b = j + 1;
+		    				 max = p;
+		    			    }
+                        }
+                    // match.compare();
+		    	}
+		    	System.out.println();
+		    }
+		    System.out.println("Maximum similarity is between " + "File" + a
+		    	+ ".txt and File" + b + ".txt");
 
-		} catch (NoSuchElementException e) {
-			System.out.println("empty directory");
-		}
-	}
+        } catch (NoSuchElementException e) {
+        	System.out.println("empty directory");
+        }
+    }
 
 }
